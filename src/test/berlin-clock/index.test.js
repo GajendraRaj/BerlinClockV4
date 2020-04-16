9a57f2f7ac213df45b112e805aad0f09a424a62e
GajendraRaj <Gajendra.rajput81@gmail.com>
2020-04-16 14:29:59 +0530

    test: Berlin Clock should return 'ORRRRRRROYYRYYRYYRYYYYYY' for '23:59:59'

--- src/test/berlin-clock/index.test.js
+++ src/test/berlin-clock/index.test.js
@@ -263,4 +263,23 @@ describe("Entire Berlin Clock functionality", () => {
 
     expect(clockTime).toEqual("ORROOROOOYYRYYRYOOOOYYOO");
   });
+
+  it("should return ORRRRRRROYYRYYRYYRYYYYYY for '23:59:59' time", () => {
+    const wrapper = shallow(<BerlinClock time={"23:59:59"} />);
+    const hoursWrapper = wrapper.find(Hours);
+    const minutesWrapper = wrapper.find(Minutes);
+    const secondsWrapper = wrapper.find(Seconds);
+
+    let clockTime;
+    const fiveHoursRow = hoursWrapper.props().hours[0];
+    const singleHoursRow = hoursWrapper.props().hours[1];
+    const fiveMinuteRow = minutesWrapper.props().minutes[0];
+    const singleMinuterow = minutesWrapper.props().minutes[1];
+    const second = secondsWrapper.props().seconds;
+
+    clockTime =
+      second + fiveHoursRow + singleHoursRow + fiveMinuteRow + singleMinuterow;
+
+    expect(clockTime).toEqual("ORRRRRRROYYRYYRYYRYYYYYY");
+  });
 });
